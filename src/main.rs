b88ab9f7f723cea27dd4a5b6a7efca1b4637fa65
Scott Kirkpatrick <scottkirk@scottkirk.dev>
2020-11-11 21:30:55 -0500

    loop experimentation

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,25 @@
 fn main() {
-    let x = {
-        let y = 4;
-        y
+    let mut counter = 0;
+    let x = loop {
+        counter += 1;
+        if counter % 11 == 0 {
+            break counter;
+        }
     };
-    // comments are usually above the line they're referencing
-    different_function(x, get_my_float());
-    if x == 3 {
-        println!("x equaled 3!");
-    } else if x % 3 == 0 {
-        println!("x is divisible by 3!");
-    } else {
-        println!("x did not equal 3 and is not divisible by it :(");
+    println!("x is now {} and y is {}", x, get_y_value());
+    let mut x = x;
+    while x != 3 {
+        x -= 1;
+    }
+    println!("x is now {}", x);
+
+    let arr = [x, 5, 4, 3, 2, 1];
+
+    for ele in arr.iter() {
+        println!("the ele is {}", ele);
     }
 }
 
-fn get_my_float() -> f64 {
-    0.1
-}
-
-fn different_function(x: i64, y: f64) {
-    println!("You passed in {} and {}", x, y);
+fn get_y_value() -> i32 {
+    12
 }
